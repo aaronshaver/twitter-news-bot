7f1381dfd66f543caa7149435b28efdc19597430
Aaron Shaver <aaronshaverpdx@gmail.com>
2016-12-31 21:05:05 -0800

    Lots of stuff - put bot in loop; better logging; print Tweepy errors; sleep between tweets, etc.

--- retweet.py
+++ retweet.py
@@ -5,6 +5,7 @@ import configparser
 import hashlib
 import inspect
 import os
+import time
 
 import tweepy
 
@@ -32,68 +33,70 @@ auth = tweepy.OAuthHandler(config.get("twitter", "consumer_key"), config.get("tw
 auth.set_access_token(config.get("twitter", "access_token"), config.get("twitter", "access_token_secret"))
 api = tweepy.API(auth)
 
-# retrieve last savepoint if available
-try:
-    with open(last_id_file, "r") as file:
-        savepoint = file.read()
-except IOError:
-    savepoint = ""
-    print("No savepoint found. Trying to get as many results as possible.")
-
-print("Creating timeline iterator...")
-timelineIterator = tweepy.Cursor(api.search, q=search_term, since_id=savepoint, lang=tweetLanguage,
-                                 wait_on_rate_limit=True, wait_on_rate_limit_notify=True).items(1500)
-
-print("Put items from tweety.Cursor object into a list for easier sorting, filtering...")
-timeline = []
-total = 0
-for status in timelineIterator:
-    timeline.append(status)
-    total += 1
-print("\n*** Total tweets found: " + str(total))
-
-# print()
-# pprint(vars(timeline[0]))
-# print()
-
-timeline.sort(key=lambda x: x.retweet_count, reverse=True)
-
-for status in timeline:
-    print("retweet_count: " + str(status.retweet_count))
-
-try:
-    last_tweet_id = timeline[0].id
-except IndexError:
-    last_tweet_id = savepoint
-
-# filter @replies/blacklisted words & users out and reverse timeline
-timeline = filter(lambda status: status.text[0] != "@", timeline)
-timeline = filter(lambda status: not any(word in status.text.split() for word in wordBlacklist), timeline)
-timeline = filter(lambda status: status.author.screen_name not in userBlacklist, timeline)
-# timeline.reverse()
-
-tw_counter = 0
-err_counter = 0
-
-print("Retweeting the most-retweeted tweet...")
-success = False
-for status in timeline:
+while True:
+    # retrieve last savepoint if available
     try:
-        print("d: (%(date)s)\n name: %(name)s\n message: %(message)s\n count: %(retweet_count)s\n" %
-              {"date": status.created_at,
-               "name": status.author.screen_name.encode('utf-8'),
-               "message": status.text.encode('utf-8'),
-               "retweet_count": status.retweet_count})
-        if config.get("settings", "retweeting_enabled") == "True":
-            api.retweet(status.id)
-        break
-        tw_counter += 1
-    except tweepy.error.TweepError as e:
-        err_counter += 1
-        continue
+        with open(last_id_file, "r") as file:
+            savepoint = file.read()
+    except IOError:
+        savepoint = ""
+        print("No savepoint found. Trying to get as many results as possible.")
 
-print("Finished. %d Tweets retweeted, %d errors occured." % (tw_counter, err_counter))
+    print("Creating timeline iterator...\n")
+    timelineIterator = tweepy.Cursor(api.search, q=search_term, since_id=savepoint, lang=tweetLanguage,
+                                     wait_on_rate_limit=True, wait_on_rate_limit_notify=True). \
+        items(int(config.get("settings", "max_tweets_to_fetch")))
 
-# write last retweeted tweet id to file
-with open(last_id_file, "w") as file:
-    file.write(str(last_tweet_id))
+    print("Put items from tweety.Cursor object into a list for easier sorting, filtering...\n")
+    timeline = []
+    total = 0
+    for status in timelineIterator:
+        timeline.append(status)
+        total += 1
+    print("Total tweets found: " + str(total) + "\n")
+
+    # print()
+    # pprint(vars(timeline[0]))
+    # print()
+
+    timeline.sort(key=lambda x: x.retweet_count, reverse=True)
+
+    try:
+        last_tweet_id = timeline[0].id
+    except IndexError:
+        last_tweet_id = savepoint
+
+    # filter @replies/blacklisted words & users out and reverse timeline
+    timeline = filter(lambda status: status.text[0] != "@", timeline)
+    timeline = filter(lambda status: not any(word in status.text.split() for word in wordBlacklist), timeline)
+    timeline = filter(lambda status: status.author.screen_name not in userBlacklist, timeline)
+    # timeline.reverse()
+
+    err_counter = 0
+
+    print("Attempting to retweet the most-retweeted tweet...\n")
+    success = False
+
+    while not success:
+        for status in timeline:
+            try:
+                if config.get("settings", "retweeting_enabled") == "True":
+                    api.retweet(status.id)
+                    print("Retweeting " + str(status.id) + " succeeded\n")
+                    success = True
+                    break
+            except tweepy.error.TweepError as e:
+                print("Error: " + e.reason + "\n")
+                err_counter += 1
+                continue
+
+    print("Finished attempting to retweet from that batch of tweets; %d errors occured" % err_counter)
+
+    # write last retweeted tweet id to file
+    with open(last_id_file, "w") as file:
+        file.write(str(last_tweet_id))
+
+    sleep_time = int(config.get("settings", "time_between_retweets"))
+    print("\nSleeping for %d seconds between retweets..." % sleep_time)
+    print("\n-------------------------------------------------------------------------------------------------------\n")
+    time.sleep(sleep_time)
