993aa2d1fc7fcc5a26232088415e9274da39d122
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-03-10 16:01:04 -0700

    Custom logger, more error details, list comprehension, etc.

--- twitter_news_bot/twitter_news_bot.py
+++ twitter_news_bot/twitter_news_bot.py
@@ -12,18 +12,24 @@ from datetime import datetime, timedelta
 
 import tweepy
 
+def setup_custom_logger(name):
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    date_time_name = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
+    handler = logging.FileHandler(date_time_name + '.log', mode='w')
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    return logger
 
 class TwitterNewsBot:
-    userBlacklist = []
-    wordBlacklist = []
-
-    @staticmethod
-    def retrieve_save_point(last_id_file):
+    def retrieve_save_point(self):
         try:
-            with open(last_id_file, "r") as saved_file:
+            with open(self.last_id_file, "r") as saved_file:
                 return saved_file.read()
         except IOError:
-            logging.info('No savepoint found. Trying to get as many results (tweets) as possible ' + get_current_time_string())
+            self.logger.info('No savepoint found. Trying to get as many results (tweets) as possible ')
             return ""
 
     def build_save_point(self):
@@ -34,9 +40,7 @@ class TwitterNewsBot:
         return last_id_file
 
     def __init__(self):
-        date_time_name = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
-        logging.basicConfig(filename=date_time_name + '.log', level=logging.INFO)
-
+        self.logger = setup_custom_logger('myapp')
         path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
         self.config = configparser.ConfigParser()
         self.config.read(os.path.join(path, "configuration.txt"))
@@ -46,7 +50,7 @@ class TwitterNewsBot:
         self.max_age_in_minutes = int(self.config.get("settings", "max_age_in_minutes"))
 
         self.last_id_file = self.build_save_point()
-        self.savepoint = self.retrieve_save_point(self.last_id_file)
+        self.savepoint = self.retrieve_save_point()
 
         auth = tweepy.OAuthHandler(self.config.get("twitter", "consumer_key"), self.config.
                                    get("twitter", "consumer_secret"))
@@ -55,77 +59,85 @@ class TwitterNewsBot:
         self.api = tweepy.API(auth)
 
     def retweet(self):
-        print("\n\n\nThe Twitter retweet bot is now running. Please see the dated .log file for output." + get_current_time_string())
+        print("\n\n\nThe Twitter retweet bot is now running. Please see the dated .log file for output.")
 
         while True:
-            logging.info("Doing the actual Twitter search..." + get_current_time_string())
+            self.logger.info("Doing the actual Twitter search...")
             timeline_iterator = tweepy.Cursor(self.api.search, q=self.search_term, since_id=self.savepoint,
                                               lang=self.tweet_language, wait_on_rate_limit=True,
                                               wait_on_rate_limit_notify=True). \
                 items(int(self.config.get("settings", "max_tweets_to_fetch")))
 
             timeline = []
-            logging.info("appending statuses to timeline..." + get_current_time_string())
+            self.logger.info("Appending statuses to timeline...")
             for status in timeline_iterator:
                 timeline.append(status)
             if len(timeline) < 1:
-                logging.error("Exiting program. Zero tweets. Something went wrong (OAuth or search had no results)" + get_current_time_string())
+                self.logger.error("Something went wrong (no tweets found).")
+                self.logger.error("Make sure OAuth is working (correct keys, etc. and networking is open if using a cloud provider).")
+                self.logger.error("It's also possible you're using search settings that are way too restrictive, like an unusual phrase or you're not fetching enough tweets.") 
+                print("\n\nError. See latest .log file for details.\n\n")
                 sys.exit()
-            logging.info("Total tweets found: " + str(len(timeline)) + get_current_time_string())
+            self.logger.info("Total tweets found: " + str(len(timeline)))
 
-            logging.info("Sorting tweets by highest retweet count...")
+            self.logger.info("Sorting tweets by highest retweet count...")
             timeline.sort(key=lambda x: x.retweet_count, reverse=True)  # put most-retweeted tweets first
 
             try:
-                logging.info("trying to get last tweet id by most recent in the bot's timeline" + get_current_time_string())
+                self.logger.info("Trying to get last tweet id by most recent in the bot's timeline")
                 last_tweet_id = timeline[0].id
             except IndexError:
-                logging.info("there was an index error, so use the savepoint instead" + get_current_time_string())
+                self.logger.info("There was an index error, so use the savepoint instead")
                 last_tweet_id = self.savepoint
 
-            logging.info("Doing filtering of tweets..." + get_current_time_string())
+            self.logger.info("Filtering of tweets...")
+            self.logger.info("Initial length of timeline: " + str(len(timeline)))
             timeline = [tweet for tweet in timeline if hasattr(tweet, "retweeted_status")]
-            timeline = filter(lambda tweet: tweet.text[0] != "@", timeline)
-            timeline = filter(lambda tweet: not any(word in tweet.text.split() for word in self.wordBlacklist),
-                              timeline)
-            timeline = filter(lambda tweet: tweet.author.screen_name not in self.userBlacklist, timeline)
-            timeline = filter(lambda tweet: tweet.retweeted_status.created_at >
-                                            (datetime.utcnow() - timedelta(minutes=self.max_age_in_minutes)), timeline)
+            self.logger.info("Length after filtering out un-retweeted tweets: " + str(len(timeline)))
+            timeline = [tweet for tweet in timeline if tweet.text[0] != "@"]  # prevents retweeting @ mention tweets
+            self.logger.info("Length after filtering out @ mention tweets: " + str(len(timeline)))
 
-            logging.info("Filtering done." + get_current_time_string())
+            for tweet in timeline:
+                self.logger.debug(tweet)
+            no_older_than_this_time = datetime.utcnow() - timedelta(minutes=self.max_age_in_minutes)
+            timeline = [tweet for tweet in timeline if tweet.retweeted_status.created_at > no_older_than_this_time]
+            self.logger.info("Length after filtering out tweets that are older than our specified max age: " + str(len(timeline)))
 
-            logging.info("Going through statuses in timeline..." + get_current_time_string())
-            success = False
-            for status in timeline:
-                try:
-                    if self.config.get("settings", "retweeting_enabled") == "True":
-                        logging.info("Attempting to retweet " + str(status.id) + "..." + get_current_time_string())
-                        self.api.retweet(status.id)
-                        logging.info("Retweeting tweet id " + str(status.id) + " succeeded!" + get_current_time_string())
-                        last_tweet_id = status.id
-                        success = True
-                        break
-                    else:
-                        logging.warning("retweeting_enabled is not True, so we do nothing instead of retweet" + get_current_time_string())
-                        break
-                except tweepy.error.TweepError as e:
-                    logging.info("Tweepy error: " + e.reason + get_current_time_string())
-                    continue
-            
-            if success == False:
-                logging.info("If retweeting continues to fail, try increasing max_tweets_to_fetch in the config file or using a different search phrase")
+            num_tweets_after_filtering = len(timeline)
+            self.logger.info("Final length of timeline: " + str(num_tweets_after_filtering))
 
-            logging.info("Writing last id file..." + get_current_time_string())
+            if self.config.get("settings", "retweeting_enabled") == "True":
+                if num_tweets_after_filtering > 0:
+                    stop_trying_to_retweet = False
+                    while not stop_trying_to_retweet:
+                        for status in timeline:
+                            self.logger.info("Going through statuses in timeline...")
+                            try:
+                                self.logger.info("Attempting to retweet " + str(status.id) + "...")
+                                self.api.retweet(status.id)
+                                self.logger.info("Retweeting tweet id " + str(status.id) + " succeeded!")
+                                last_tweet_id = status.id
+                                stop_trying_to_retweet = True
+                                break
+                            except tweepy.error.TweepError as e:
+                                self.logger.info("Tweepy error: " + e.reason())
+                                continue
+                            except:
+                                self.logger.error("Unexpected error: ", sys.exc_info()[0])
+                                raise
+                else:
+                    self.logger.warning("There were no tweets left after filtering")
+                    self.logger.warning("Hints: try increasing max_tweets_to_fetch, or increasing max_age_in_minutes, or use a different search phrase")
+            else:
+                self.logger.warning("retweeting_enabled is not True, so we do nothing instead of retweet")
+
+            self.logger.info("Writing last id file...")
             with open(self.last_id_file, "w") as file:
                 file.write(str(last_tweet_id))
 
-            logging.info("Now sleeping for %d seconds between retweets" % self.sleep_time)
-            logging.info("It is now" + get_current_time_string())
-            logging.info("------------------------------------------------------------------------------")
+            self.logger.info("Now sleeping for %d seconds between retweets" % self.sleep_time)
+            self.logger.info("------------------------------------------------------------------------------")
             time.sleep(self.sleep_time)
-
-def get_current_time_string():
-    return " [" + datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S") + "]"
 
 if __name__ == "__main__":
     bot = TwitterNewsBot()
